8cbc3cd9a490d18d682db28f6bc477a498bc7085
Kostas Minaidis <kostas@creativecollective.gr>
2015-07-08 17:27:44 +0300

    Adding external config.php

--- api/config.php
+++ api/config.php
@@ -4,17 +4,17 @@
  * Your Twitter App Info
  */
 
-// Consumer Key
-define('CONSUMER_KEY', 'CONSUMER_KEY_HERE');
-define('CONSUMER_SECRET', 'CONSUMER_SECRET_HERE');
+require("../config.php");
 
-// User Access Token
-define('ACCESS_TOKEN', 'ACCESS_TOKEN_HERE');
-define('ACCESS_SECRET', 'ACCESS_SECRET_HERE');
+// CONSUMER KEY
+define('CONSUMER_KEY', $themeConfig['TWITTER']['CONSUMER_KEY']);
+define('CONSUMER_SECRET', $themeConfig['TWITTER']['CONSUMER_SECRET']);
 
-// Cache Settings
+// USER ACCESS TOKEN 
+define('ACCESS_TOKEN', $themeConfig['TWITTER']['ACCESS_TOKEN']);
+define('ACCESS_SECRET', $themeConfig['TWITTER']['ACCESS_SECRET']);
+
+// CACHE SETTINGS
 define('CACHE_ENABLED', false);
 define('CACHE_LIFETIME', 3600); // in seconds
 define('HASH_SALT', md5(dirname(__FILE__)));
-
-
